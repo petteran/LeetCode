846ffd08a03dbf1a4d46a754691905149229a196
petteran <petteran.h@qq.com>
2022-01-12 23:31:37 +0800

    题目: 买卖股票的最佳时机

--- src/easy/买卖股票的最佳时机/Solution.java
+++ src/easy/买卖股票的最佳时机/Solution.java
@@ -9,7 +9,7 @@ package easy.买卖股票的最佳时机;
  */
 
 /**
- * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ * 给定一个数组 prices ，它的第i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
  *
  * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
  *
@@ -23,15 +23,15 @@ public class Solution {
         System.out.println(i);
     }
     public static int maxProfit(int[] prices) {
-        int minprice = Integer.MAX_VALUE;
-        int maxprofit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
         for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < minprice) {
-                minprice = prices[i];
-            } else if (prices[i] - minprice > maxprofit) {
-                maxprofit = prices[i] - minprice;
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
             }
         }
-        return maxprofit;
+        return maxProfit;
     }
 }
