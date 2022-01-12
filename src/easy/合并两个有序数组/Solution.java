9537a47ef35851daf958025b5b5f72ae63388e85
petteran <petteran.h@qq.com>
2022-01-12 23:32:09 +0800

    题目: 合并两个有序数组

--- src/easy/合并两个有序数组/Solution.java
+++ src/easy/合并两个有序数组/Solution.java
@@ -18,7 +18,6 @@ import java.util.stream.Collectors;
  * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
  *
  * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
- *
  */
 public class Solution {
     public static void main(String[] args) {
