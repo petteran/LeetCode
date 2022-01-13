a932c71efbfecbc77024d290afb373dcb2de0fbb
petteran <petteran.h@qq.com>
2022-01-14 00:26:44 +0800

    题目: 有效的数独

--- src/easy/有效的数独/Solution.java
+++ src/easy/有效的数独/Solution.java
@@ -2,6 +2,8 @@ package easy.有效的数独;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 项目名称：LeetCode
@@ -10,40 +12,51 @@ import java.util.HashSet;
  * 创建时间：2022/1/13 22:26
  * 创 建 人：petteran
  */
+
+/**
+ * 请你判断一个9 x 9 的数独是否有效。只需要 根据以下规则 ，验证已经填入的数字是否有效即可。
+ * <p>
+ * 数字1-9在每一行只能出现一次。
+ * 数字1-9在每一列只能出现一次。
+ * 数字1-9在每一个以粗实线分隔的 3x3 宫内只能出现一次。（请参考示例图）
+ */
 public class Solution {
     public static void main(String[] args) {
         char[][] arr =
                 {
-                        {'7','.','.','.','4','.','.','.','.'},
-                        {'.','.','.','8','6','5','.','.','.'},
-                        {'.','1','.','2','.','.','.','.','.'},
-                        {'.','.','.','.','.','9','.','.','.'},
-                        {'.','.','.','.','5','.','5','.','.'},
-                        {'.','.','.','.','.','.','.','.','.'},
-                        {'.','.','.','.','.','.','2','.','.'},
-                        {'.','.','.','.','.','.','.','.','.'},
-                        {'.','.','.','.','.','.','.','.','.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '1', '.'},
+                        {'.', '4', '.', '3', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '3', '.', '.', '1'},
+                        {'8', '.', '.', '.', '.', '.', '.', '2', '.'},
+                        {'.', '.', '2', '.', '7', '.', '.', '.', '.'},
+                        {'.', '1', '5', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '2', '.', '.', '.'},
+                        {'.', '2', '.', '9', '.', '.', '.', '.', '.'},
+                        {'.', '.', '4', '.', '.', '.', '.', '.', '.'},
                 };
 
         boolean validSudoku = isValidSudoku(arr);
         System.out.println(validSudoku);
 
     }
+
     public static boolean isValidSudoku(char[][] board) {
-        for (int i = 0; i <9 ; i++) {
-            HashSet<Integer> ColSet = new HashSet<>();
-            HashSet<Integer> RowSet = new HashSet<>();
-            for (int j = 0; j <9 ; j++) {
-                int c1 = board[j][i] == '.' ? 10 : board[j][i] - '1';
-                if (c1 != 10) {
-                    boolean add = ColSet.add(c1);
-                    if (!add){ return false;}
-                }
-                int c =board[i][j] == '.' ? 10 : board[i][j] - '1';
-                if (c != 10) {
-                    boolean add = RowSet.add(c);
-                    if (!add){return false;}
-                }
+        Map<Integer, Set<Integer>> row = new HashMap<>(), col = new HashMap<>(), area = new HashMap<>();
+        for (int i = 0; i < 9; i++) {
+            row.put(i, new HashSet<>());
+            col.put(i, new HashSet<>());
+            area.put(i, new HashSet<>());
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
+                int u = c - '0';
+                int idx = i / 3 * 3 + j / 3;
+                if (row.get(i).contains(u) || col.get(j).contains(u) || area.get(idx).contains(u)) return false;
+                row.get(i).add(u);
+                col.get(j).add(u);
+                area.get(idx).add(u);
             }
         }
         return true;
