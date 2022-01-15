c617342c5840b4759ddec52cc2b702eaaaf26a16
petteran <petteran.h@qq.com>
2022-01-15 23:21:48 +0800

    题目: 环形链表

---
+++ src/easy/环形链表/ListNode.java
@@ -0,0 +1,17 @@
+package easy.环形链表;
+
+/**
+ * 项目名称：LeetCode
+ * 类 名 称：ListNode
+ * 类 描 述：单链表
+ * 创建时间：2022/1/15 16:49
+ * 创 建 人：petteran
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
