be12d3e9ff349bb4f3975f2f1f6b4564cef05d85
petteran <petteran.h@qq.com>
2022-01-19 22:33:41 +0800

    题目: 二叉树的层序遍历;

---
+++ src/easy/二叉树遍历/层序遍历/Solution.java
@@ -0,0 +1,49 @@
+package easy.二叉树遍历.层序遍历;
+
+/**
+ * 项目名称：LeetCode
+ * 类 名 称：Solution
+ * 类 描 述：二叉树的层序遍历
+ * 创建时间：2022/1/19 22:21
+ * 创 建 人：petteran
+ */
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。
+ * （即逐层地，从左到右访问所有节点）。
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (null == root) {
+            return res;
+        }
+        LinkedList<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        // while 循环控制从上向下一层层遍历
+        while (!q.isEmpty()) {
+            int size = q.size();
+            // 记录这一层的节点值
+            List<Integer> level = new LinkedList<>();
+            // for 循环控制每一层从左向右遍历
+            for (int i = 0; i <size ; i++) {
+                TreeNode poll = q.poll();
+                level.add(poll.val);
+                if (poll.left != null) {
+                    q.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    q.offer(poll.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
