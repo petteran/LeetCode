fc75d5ae097c83eadb681ccc7482289ec9374ac3
petteran <petteran.h@qq.com>
2022-01-22 23:35:58 +0800

    题目: 验证二叉搜索树;

---
+++ src/easy/二叉树遍历/验证二叉搜索树/Solution.java
@@ -0,0 +1,30 @@
+package easy.二叉树遍历.验证二叉搜索树;
+
+/**
+ * 项目名称：LeetCode
+ * 类 名 称：Solution
+ * 类 描 述：验证二叉搜索树
+ * 创建时间：2022/1/22 23:33
+ * 创 建 人：petteran
+ */
+
+
+/**
+ * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ *
+ * 有效 二叉搜索树定义如下：
+ *
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ *
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public boolean isValidBST(TreeNode root) {
+
+        return false;
+    }
+}
---
+++ src/easy/二叉树遍历/验证二叉搜索树/TreeNode.java
@@ -0,0 +1,21 @@
+package easy.二叉树遍历.验证二叉搜索树;
+
+/**
+ * 项目名称：LeetCode
+ * 类 名 称：TreeNode
+ * 类 描 述：a binary tree node
+ * 创建时间：2022/1/19 22:34
+ * 创 建 人：petteran
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
