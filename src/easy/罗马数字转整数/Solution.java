3a1970730902c8e199dddb43e9e30924188a0b04
petteran <petteran.h@qq.com>
2022-01-23 14:43:37 +0800

    题目: 罗马数字转整数;

---
+++ src/easy/罗马数字转整数/Solution.java
@@ -0,0 +1,80 @@
+package easy.罗马数字转整数;
+
+/**
+ * 项目名称：LeetCode
+ * 类 名 称：Solution
+ * 类 描 述：罗马数字转整数
+ * 创建时间：2022/1/23 14:27
+ * 创 建 人：petteran
+ */
+
+/**
+ * 罗马数字包含以下七种字符:I，V，X，L，C，D 和 M。
+ * <p>
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * 例如， 罗马数字 2 写做 II，即为两个并列的 1 。12 写做 XII，即为 X + II。 27 写做 XXVII, 即为 XX + V + II。
+ * <p>
+ * 通常情况下，罗马数字中小的数字在大的数字的右边。
+ * 但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。
+ * 同样地，数字 9 表示为IX。这个特殊的规则只适用于以下六种情况：
+ * <p>
+ * I 可以放在 V(5) 和 X(10) 的左边，来表示 4 和 9。
+ * X 可以放在 L(50) 和 C(100) 的左边，来表示 40 和 90。
+ * C 可以放在 D(500) 和 M(1000) 的左边，来表示 400 和 900。
+ * 给定一个罗马数字，将其转换成整数。
+ */
+public class Solution {
+    public int romanToInt(String s) {
+        s = s.replace("IV", "a");
+        s = s.replace("IX", "b");
+        s = s.replace("XL", "c");
+        s = s.replace("XC", "d");
+        s = s.replace("CD", "e");
+        s = s.replace("CM", "f");
+
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            result += change(s.charAt(i));
+        }
+        return result;
+    }
+
+    public int change(char ch) {
+        switch (ch) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            case 'a':
+                return 4;
+            case 'b':
+                return 9;
+            case 'c':
+                return 40;
+            case 'd':
+                return 90;
+            case 'e':
+                return 400;
+            case 'f':
+                return 900;
+        }
+        return 0;
+    }
+}
