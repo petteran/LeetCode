650552108b2ed48efe463ddbca8ef2a780de99c9
petteran <petteran.h@qq.com>
2022-02-14 21:34:09 +0800

    题目: 二进制求和;

---
+++ src/easy/二进制求和/Solution.java
@@ -0,0 +1,37 @@
+package easy.二进制求和;
+
+/**
+ * 项目名称：LeetCode
+ * 类 名 称：Solution
+ * 类 描 述：二进制求和
+ * 创建时间：2022/2/14 21:23
+ * 创 建 人：petteran
+ */
+
+
+/**
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+ *
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ */
+public class Solution {
+    public String addBinary(String a, String b) {
+        StringBuffer ans = new StringBuffer();
+
+        int n = Math.max(a.length(), b.length()), carry = 0;
+        for (int i = 0; i < n; ++i) {
+            carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+            carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+            ans.append((char) (carry % 2 + '0'));
+            carry /= 2;
+        }
+
+        if (carry > 0) {
+            ans.append('1');
+        }
+        ans.reverse();
+
+        return ans.toString();
+
+    }
+}
